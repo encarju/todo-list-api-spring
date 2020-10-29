63feda28070e2cb8f2c1c791863060469ab46482
encarju <justine.encarnacion@oocl.com>
2020-10-29 13:57:30 +0800

    [ToDoListAPI][John-Justine] Feature: Add DeleteMapping in TodoListController

--- src/main/java/com/oocl/todolistapi/controller/TodoListController.java
+++ src/main/java/com/oocl/todolistapi/controller/TodoListController.java
@@ -1,13 +1,13 @@
 package com.oocl.todolistapi.controller;
 
 import com.oocl.todolistapi.model.Todo;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -43,5 +43,14 @@ public class TodoListController {
 
                     return todo;
                 }).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTodo(@PathVariable int id) {
+        todos.stream()
+                .filter(todo -> todo.getId() == id)
+                .findFirst()
+                .map(todo -> todos.remove(todo))
+                .orElse(null);
     }
 }
