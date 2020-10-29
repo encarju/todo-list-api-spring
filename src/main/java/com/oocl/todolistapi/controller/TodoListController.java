7a3625e77bf8baf8998b11318c821e7fddfe1605
encarju <justine.encarnacion@oocl.com>
2020-10-29 13:49:34 +0800

    [ToDoListAPI][John-Justine] Feature: Add PutMapping in TodoListController

--- src/main/java/com/oocl/todolistapi/controller/TodoListController.java
+++ src/main/java/com/oocl/todolistapi/controller/TodoListController.java
@@ -2,9 +2,12 @@ package com.oocl.todolistapi.controller;
 
 import com.oocl.todolistapi.model.Todo;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,9 +27,21 @@ public class TodoListController {
     }
 
     @PostMapping
-    public Todo addTodo(@RequestBody Todo todo){
+    public Todo addTodo(@RequestBody Todo todo) {
         todos.add(todo);
 
         return todo;
+    }
+
+    @PutMapping("/{id}")
+    public Todo updateTodoStatus(@PathVariable int id, @RequestBody Todo updatedTodo) {
+        return todos.stream()
+                .filter(todo -> todo.getId() == id)
+                .findFirst()
+                .map(todo -> {
+                    todo.setDone(updatedTodo.isDone());
+
+                    return todo;
+                }).orElse(null);
     }
 }
