1fdae54a1b34096742fb610bc77938e805effc3d
encarju <justine.encarnacion@oocl.com>
2020-10-29 20:06:28 +0800

    [ToDoListAPI][John-Justine] Feature: Add ToDoList CORS

---
+++ src/main/java/com/oocl/todolistapi/config/TodoListConfiguration.java
@@ -0,0 +1,22 @@
+package com.oocl.todolistapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class TodoListConfiguration {
+    @Bean
+    public WebMvcConfigurer configurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+
+}
